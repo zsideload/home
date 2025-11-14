165aba2e2d4e034fa5856755a9fc4f0a3f0745ba
gx8z <86722990+gx8z@users.noreply.github.com>
2025-11-14 15:52:08 +0700

    update to use SCRIPT_GITHUB_TOKEN

--- scripts/lib/github.ts
+++ scripts/lib/github.ts
@@ -1,7 +1,7 @@
 import process from "node:process";
 import { Octokit } from "octokit";
 
-const GH_TOKEN = process.env.GH_TOKEN;
-if (!GH_TOKEN) throw new Error("GH_TOKEN missing");
+const SCRIPT_GITHUB_TOKEN = process.env.SCRIPT_GITHUB_TOKEN;
+if (!SCRIPT_GITHUB_TOKEN) throw new Error("SCRIPT_GITHUB_TOKEN missing");
 
-export const octokit = new Octokit({ auth: GH_TOKEN });
+export const octokit = new Octokit({ auth: SCRIPT_GITHUB_TOKEN });
