e551ac2cffa6ba5cffc3fd2996c3014dd81e3e16
gx8z <86722990+gx8z@users.noreply.github.com>
2025-11-14 12:19:26 +0700

    update compare helper

--- scripts/lib/compare.ts
+++ scripts/lib/compare.ts
@@ -4,3 +4,6 @@ const collator = new Intl.Collator(undefined, {
 });
 
 export const aNewerThanB = (a: string, b: string) => collator.compare(a, b) > 0;
+
+export const sortAsc = (a: string, b: string) => collator.compare(a, b);
+export const sortDesc = (a: string, b: string) => collator.compare(b, a);
