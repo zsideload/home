ba416f10374d5f73d07bfc250016bfb4cc4ed9d1
gx8z <86722990+gx8z@users.noreply.github.com>
2025-11-19 21:37:27 +0700

    rename scripts token

--- scripts/lib/github.ts
+++ scripts/lib/github.ts
@@ -1,7 +1,7 @@
 import process from "node:process";
 import { Octokit } from "octokit";
 
-const SCRIPT_GITHUB_TOKEN = process.env.SCRIPT_GITHUB_TOKEN;
-if (!SCRIPT_GITHUB_TOKEN) throw new Error("SCRIPT_GITHUB_TOKEN missing");
+const SCRIPTS_GITHUB_TOKEN = process.env.SCRIPTS_GITHUB_TOKEN;
+if (!SCRIPTS_GITHUB_TOKEN) throw new Error("SCRIPTS_GITHUB_TOKEN missing");
 
-export const octokit = new Octokit({ auth: SCRIPT_GITHUB_TOKEN });
+export const octokit = new Octokit({ auth: SCRIPTS_GITHUB_TOKEN });
